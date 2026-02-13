09923a90ebf0873476dcb29cbf6ffb68c7cfac86
SluShY0609 <cedric609.waibel@gmail.com>
2026-02-13 08:40:12 +0100

    Make seed data idempotent with MERGE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
--- Länder (IDs werden automatisch vergeben)
-INSERT INTO country (iso_code, name) VALUES ('DEU', 'Deutschland');
-INSERT INTO country (iso_code, name) VALUES ('FRA', 'Frankreich');
-INSERT INTO country (iso_code, name) VALUES ('USA', 'Vereinigte Staaten');
+-- Länder (idempotent über KEY(iso_code))
+MERGE INTO country (iso_code, name) KEY(iso_code) VALUES ('DEU', 'Deutschland');
+MERGE INTO country (iso_code, name) KEY(iso_code) VALUES ('FRA', 'Frankreich');
+MERGE INTO country (iso_code, name) KEY(iso_code) VALUES ('USA', 'Vereinigte Staaten');
 
--- Emissionen (wir referenzieren über SELECT auf das Land)
-INSERT INTO emission_record (country_id, year_value, co2_kilotons)
+-- Emissionen (idempotent über zusammengesetzten Key: country_id + year_value)
+MERGE INTO emission_record (country_id, year_value, co2_kilotons) KEY(country_id, year_value)
 VALUES ((SELECT id FROM country WHERE iso_code='DEU'), 2021, 650000.0);
 
-INSERT INTO emission_record (country_id, year_value, co2_kilotons)
+MERGE INTO emission_record (country_id, year_value, co2_kilotons) KEY(country_id, year_value)
 VALUES ((SELECT id FROM country WHERE iso_code='DEU'), 2022, 640000.0);
 
-INSERT INTO emission_record (country_id, year_value, co2_kilotons)
+MERGE INTO emission_record (country_id, year_value, co2_kilotons) KEY(country_id, year_value)
 VALUES ((SELECT id FROM country WHERE iso_code='FRA'), 2022, 300000.0);
 
-INSERT INTO emission_record (country_id, year_value, co2_kilotons)
+MERGE INTO emission_record (country_id, year_value, co2_kilotons) KEY(country_id, year_value)
 VALUES ((SELECT id FROM country WHERE iso_code='USA'), 2021, 5000000.0);
 
-INSERT INTO emission_record (country_id, year_value, co2_kilotons)
+MERGE INTO emission_record (country_id, year_value, co2_kilotons) KEY(country_id, year_value)
 VALUES ((SELECT id FROM country WHERE iso_code='USA'), 2022, 5100000.0);
